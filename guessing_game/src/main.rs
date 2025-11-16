33bfd15629257af74955cb08836c9a61bd3a3c7f
42Questions <michael.summerville@gmail.com>
2025-11-16 14:47:20 +0000

    My first rust project

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,45 @@
+// Importing io from the standard library
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    // Detail this thread is local to the current execution context
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=10);
+    loop {
+        println!("Please input your guess.");
+        
+        // Creating a mutable String variable to store the user's guess
+        // Without mut, we would not be able to change the value of guess
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        // Convert guess from String to i32
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // _ is a catch-all pattern that matches any value
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+        
+        println!("You guessed: {}", guess);
+
+        // Compare the guess with the secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+    
+    println!("The secret number is: {}", secret_number);
+}
